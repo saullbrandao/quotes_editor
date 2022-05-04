48f1f5bc726189a28be5915b4ecf9cab4f56db93
Saull Brandão <saull@outlook.com>
2022-05-04 09:01:47 -0300

    Add line items nested in line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'line_items/new'
+  get 'line_items/create'
+  get 'line_items/edit'
+  get 'line_items/update'
+  get 'line_items/destroy'
   root to: "pages#home"
   
   devise_for :users
   
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
